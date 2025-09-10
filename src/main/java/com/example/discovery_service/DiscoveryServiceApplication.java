c4cdac1835bb361460e2591be499f0db2d2d45c0
mahima <dodmahima@gmail.com>
2025-09-10 21:58:34 +0530

    logger added + kibana changes added

--- src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
+++ src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
@@ -1,13 +1,20 @@
 package com.example.discovery_service;
 
+import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServiceApplication {
 	public static void main(String[] args) {
+		org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DiscoveryServiceApplication.class);
+		MDC.put("service", "book-service");
+		logger.info("Application started : Discovery service" );
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
 
