ebfbd2999ef62a8bd714c10d3ae0543b80d32ecb
mahima <dodmahima@gmail.com>
2025-09-11 12:13:20 +0530

    logger added

--- src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
+++ src/main/java/com/example/discovery_service/DiscoveryServiceApplication.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class DiscoveryServiceApplication {
 	public static void main(String[] args) {
 		org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(DiscoveryServiceApplication.class);
-		//MDC.put("service", "book-service");
+		MDC.put("service", "discovery-service");
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 		logger.info("Application started : Discovery service" );
 	}
